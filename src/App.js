047d8d02f1e32e8878e7ef396d6196063555bed9
Felipe Mendes <felipemadruga@gmail.com>
2023-01-27 16:44:27 -0300

    Conclui requisito 03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   // state = {
@@ -44,6 +45,17 @@ class App extends React.Component {
           isSaveButtonDisabled
           onInputChange
           onSaveButtonClick
+        />
+
+        <Card 
+          cardName
+          cardDescription
+          cardAttr1
+          cardAttr2
+          cardAttr3
+          cardImage
+          cardRare
+          cardTrunfo
         />
       </>
     );
---
+++ src/components/Card.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <>
+      <h2 data-testid="name-card">
+        Nome da Carta
+        { cardName }
+      </h2>
+      <img 
+        src={ cardImage }
+        alt={ cardName }
+        data-testid="image-card"
+      />
+      <p data-testid="description-card">
+        { cardDescription }
+      </p>
+      <p data-testid="attr1-card">
+        { cardAttr1 }
+      </p>
+      <p data-testid="attr2-card">
+        { cardAttr2 }
+      </p>
+      <p data-testid="attr3-card">
+        { cardAttr3 }
+      </p>
+      <h3 data-testid="rare-card">
+        { cardRare }
+      </h3>
+      
+      { 
+      cardTrunfo 
+        ? <p data-testid="trunfo-card">Super Trunfo</p> 
+        : ''
+      }
+      
+      </>      
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: Proptypes.string,
+  cardDescription: Proptypes.string,
+  cardAttr1: Proptypes.string,
+  cardAttr2: Proptypes.string,
+  cardAttr3: Proptypes.string,
+  cardImage: Proptypes.string,
+  cardRare: Proptypes.string,
+  cardTrunfo: Proptypes.bool,
+}.isRequired;
+
+export default Card;
